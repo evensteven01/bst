550043f4edaef03d96d3b97042c7efc3c7cca979
Steven Ford <evensteven01@gmail.com>
2022-02-19 16:07:56 -0500

    Add tests for adding to BST. Still fail

--- test/test_bst.py
+++ test/test_bst.py
@@ -15,3 +15,27 @@ def test_bst_add_one():
 	bst = BinarySearchTree()
 	bst.add(3)
 	assert str(bst) == '3 _ _ '
+
+def test_add_root_l_l():
+	""" Check that adding 3 nodes place properly in root then left, left """
+	bst = BinarySearchTree()
+	bst.add(5)
+	bst.add(4)
+	bst.add(2)
+	assert str(bst) == '5 4 _ 2 _ '
+
+def test_add_root_r_r():
+	""" Check that adding 3 nodes place properly in root then right, right """
+	bst = BinarySearchTree(5)
+	bst.add(5)
+	bst.add(6)
+	bst.add(10)
+	assert str(bst) == '5 _ 6 _ 10 '
+
+def test_add_root_duplicate():
+	""" Check that adding duplicate results in not adding the node """
+	bst = BinarySearchTree()
+	bst.add(5)
+	result = bst.add(5)
+	assert result == False
+	assert str(bst) == '5 _ _ '
