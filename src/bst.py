0b454b1c0072e082c6d42c2c18c329ac2f3d8dee
Steven Ford <evensteven01@gmail.com>
2022-02-19 13:46:55 -0500

    Implement initializing BST. Also add a __str__ method. Test initialization

--- src/bst.py
+++ src/bst.py
@@ -2,11 +2,18 @@ class BinarySearchTree:
 	""" This is a binary search tree supporting integers. """
 	def __init__(self, root: int=None):
 		""" Create a binary search tree """
-		pass
+		self.value = None
+		self.left = None
+		self.right = None
+		print(f'Initializing BST')
+		if root:
+			self.add(root)
 
 	def add(self, value: int) -> bool:
 		""" Add a value to the binary search tree """
-		pass
+		if self.value == None:
+			self.value = value
+		print(f'Adding {value}')
 
 	def remove(self, value: int) -> bool:
 		""" Remove a value from the binary search tree """
@@ -22,7 +29,18 @@ class BinarySearchTree:
 
 	def __str__(self) -> str:
 		""" Return a simple string representation of the binary search tree """
-		return "BST"
+		if not self.value:
+			return '_'
+		bstStr = str(self.value) + ' '
+		if not self.left:
+			bstStr += '_ '
+		if not self.right:
+			bstStr += '_ '
+		if self.left:
+			bstStr += str(self.left)
+		if self.right:
+			bstStr += str(self.right)
+		return bstStr
 
 if __name__ == '__main__':
 	bst = BinarySearchTree()
--- test/test_bst.py
+++ test/test_bst.py
@@ -1,5 +1,17 @@
 from src.bst import BinarySearchTree
 
 def test_bst_empty():
+	""" Check that a BST starts off empty """
 	bst = BinarySearchTree()
-	assert str(bst) == 'BST'
+	assert str(bst) == '_'
+
+def test_bst_initialize():
+	""" Check that we can initialize a BST with a value """
+	bst = BinarySearchTree(3)
+	assert str(bst) == '3 _ _ '
+
+def test_bst_initialize():
+	""" Check that we can initialize a BST, then add a value """
+	bst = BinarySearchTree()
+	bst.add(3)
+	assert str(bst) == '3 _ _ '
